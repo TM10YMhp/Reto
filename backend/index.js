2de17809db5513a22af1d8a1d8b49cb2ee68dde5
tm10ymhp <tm10ymhp@protonmail.com>
2023-01-01 18:13:53 -0500

    metodo asincrono

--- backend/index.js
+++ backend/index.js
@@ -12,12 +12,32 @@ app.listen(PORT, () => {
   console.log('http://localhost:', PORT)
 })
 
+function cifrarAsync(number) {
+  new Promise(resolve => {
+    console.time('cifrarAsync >>>')
+    const arr = number.toString().split('')
+
+    for (const i in arr) {
+      arr[i] = (Number(arr[i]) + 7) % 10
+    }
+
+    ;[arr[2], arr[3], arr[0], arr[1]] = [arr[0], arr[1], arr[2], arr[3]]
+
+    console.log(`output: ${arr.join('')}`)
+    console.timeEnd('cifrarAsync >>>')
+    resolve()
+  })
+}
+
 console.log('-----------')
 cifrar(1234)
 cifrar(4567)
 console.log('-----------')
 cifrarRapido(1234)
 cifrarRapido(4567)
+console.log('-----------')
+cifrarAsync(1234)
+cifrarAsync(4567)
 console.log('-----------')
 descifrar(0189)
 descifrar(3412)
